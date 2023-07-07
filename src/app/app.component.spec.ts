e0028f12534f72aea778b81d3cd90b8a1dc5d4b5
Brunna Serafina <brunnaserafina@gmail.com>
2023-07-06 21:17:34 -0300

    test: updated unit tests for the component app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'challenge-sistema-info'`, () => {
+  it(`should have as title 'system'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('challenge-sistema-info');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('challenge-sistema-info app is running!');
+    expect(app.title).toEqual('system');
   });
 });
